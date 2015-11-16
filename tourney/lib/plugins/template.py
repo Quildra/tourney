11f66f593b874b40ef674f8abf1ae4f3ecee48e8
Quildra <Quildra@gmail.com>
2015-11-16 23:15:13 +0000

    Added Mako, MakoTool and MakoTemplateLookup
    These are from an example, altered to work with cherrypy 3.3.0

---
+++ tourney/lib/plugins/template.py
@@ -0,0 +1,43 @@
+import tempfile
+import sys
+
+import cherrypy
+from cherrypy.process import plugins
+from mako.lookup import TemplateLookup
+
+__all__ = ['MakoTemplatePlugin']
+
+class MakoTemplatePlugin(plugins.SimplePlugin):
+    """A WSPBus plugin that manages Mako templates"""
+
+    def __init__(self, bus, base_dir=None, base_cache_dir=None,
+                 collection_size=50, encoding='utf-8'):
+        print('MakoTemplatePlugin __init__')
+        plugins.SimplePlugin.__init__(self, bus)
+        self.base_dir = base_dir
+        self.base_cache_dir = base_cache_dir or tempfile.gettempdir()
+        self.encoding = encoding
+        self.collection_size = collection_size
+        self.lookup = None
+
+    def subscribe(self):
+        print('MakoTemplatePlugin starting')
+        self.bus.log('Setting up Mako resources')
+        self.lookup = TemplateLookup(directories=self.base_dir,
+                                     module_directory=self.base_cache_dir,
+                                     input_encoding=self.encoding,
+                                     output_encoding=self.encoding,
+                                     collection_size=self.collection_size)
+        self.bus.subscribe("lookup-template", self.get_template)
+
+    def unsubscribe(self):
+        self.bus.log('Freeing up Mako resources')
+        self.bus.unsubscribe("lookup-template", self.get_template)
+        self.lookup = None
+
+    def get_template(self, name):
+        """
+        Returns Mako's template by name.
+        """
+        print('get_template' + name)
+        return self.lookup.get_template(name)
