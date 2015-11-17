bc13a6294c0c161e470b7e84b1044d8104fc72b5
Quildra <Quildra@gmail.com>
2015-11-17 22:07:02 +0000

    tidied up a bit
    removed prints that were added while trying to track down problems.

--- tourney/lib/tools/db.py
+++ tourney/lib/tools/db.py
@@ -12,31 +12,29 @@ class SATool(cherrypy.Tool):
         we use the scoped_session that will create a session
         on a per thread basis so that you don't worry about
         concurrency on the session object itself.
- 
+
         This tools binds a session to the engine each time
         a requests starts and commits/rollbacks whenever
         the request terminates.
         """
-        print("SATool, init")
         cherrypy.Tool.__init__(self, 'on_start_resource',
                                self.bind_session,
                                priority=20)
- 
+
     def _setup(self):
         cherrypy.Tool._setup(self)
         cherrypy.request.hooks.attach('on_end_resource',
                                       self.commit_transaction,
                                       priority=80)
- 
+
     def bind_session(self):
         """
         Attaches a session to the request's scope by requesting
         the SA plugin to bind a session to the SA engine.
         """
-        print("bind session")
         session = cherrypy.engine.publish('bind-session').pop()
         cherrypy.request.db = session
- 
+
     def commit_transaction(self):
         """
         Commits the current transaction or rolls back
